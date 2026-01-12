4dd3bd0aaaf4fd32b243f514631b89d9f96e1478
Jonathan Lara <jonathan.lara@unmsm.edu.pe>
2026-01-12 17:13:04 -0500

    Fix default database port in TypeOrmModule configuration from 3000 to 5432

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT || '3000', 10),
+      port: parseInt(process.env.DB_PORT || '5432', 10),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
